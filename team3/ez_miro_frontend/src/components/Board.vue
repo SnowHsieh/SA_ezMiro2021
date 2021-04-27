a992eabff4f622ef0d4b1df1a94b4dc3fac0082a
LaAaa <f98989000@gmail.com>
2021-04-27 16:32:54 +0800

    Team3 added frontend project

---
+++ team3/ez_miro_frontend/src/components/Board.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <button @click="drawARect">畫圖</button>
+        <canvas ref='board'></canvas>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      canvasContext: null
+    }
+  },
+  mounted () {
+    this.initCanvas()
+    this.drawARect()
+  },
+  methods: {
+    initCanvas () {
+      const canvas = this.$refs.board
+      canvas.width = window.innerWidth
+      canvas.height = window.innerHeight - 60
+      const ctx = canvas.getContext('2d')
+      ctx.lineCap = 'round'
+      ctx.lineJoin = 'round'
+      this.canvasContext = ctx
+    },
+    drawARect () {
+      const ctx = this.canvasContext
+      ctx.fillStyle = '#000000'
+      ctx.fillRect(10, 10, 100, 100)
+      ctx.fillStyle = 'red'
+      ctx.fillRect(20, 20, 30, 30)
+    }
+  }
+}
+</script>
