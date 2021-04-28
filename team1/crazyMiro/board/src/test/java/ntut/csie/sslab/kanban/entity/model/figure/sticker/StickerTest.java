dfc2ba0e2305c3badfbeda9d7237c13dea67d7aa
SA_team1 <t109598084@ntut.org.tw>
2021-04-28 22:30:21 +0800

    Added unit test for move of Sticker.

--- team1/crazyMiro/board/src/test/java/ntut/csie/sslab/kanban/entity/model/figure/sticker/StickerTest.java
+++ team1/crazyMiro/board/src/test/java/ntut/csie/sslab/kanban/entity/model/figure/sticker/StickerTest.java
@@ -8,6 +8,7 @@ import java.util.Random;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StickerTest {
 
@@ -28,5 +29,23 @@ public class StickerTest {
         sticker.changeContent(newContent);
 
         assertEquals(newContent, sticker.getContent());
+    }
+
+    @Test
+    public void move_position() {
+        String boardId = UUID.randomUUID().toString();
+        String stickerId = UUID.randomUUID().toString();
+        String content = "stickerCreated";
+        int size = 10;
+        String color = "black";
+        long x = new Random().nextLong();
+        long y = new Random().nextLong();
+        Coordinate position = new Coordinate(x, y);
+        Sticker sticker = new Sticker(boardId, stickerId, content, size, color, position);
+        Coordinate newPosition = new Coordinate(new Random().nextLong(), new Random().nextLong());
+
+        sticker.move(newPosition);
+
+        assertTrue(newPosition.equals(sticker.getPosition()));
     }
 }
