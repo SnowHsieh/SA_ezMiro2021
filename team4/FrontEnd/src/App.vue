f44de4bfcefdf9f6facc165bde777d92b720d35a
william-mac <wall1350@gmail.com>
2021-04-30 11:36:17 +0800

    Team4 adjusted front end

--- team4/FrontEnd/src/App.vue
+++ team4/FrontEnd/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Board/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Board from './components/Board.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Board
   }
 }
 </script>
