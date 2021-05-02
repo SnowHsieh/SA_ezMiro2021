35faf702ac838892deb55cc543fa8fe7bfd9ea9e
TedLin <t109598073@ntut.org.tw>
2021-05-02 20:25:58 +0800

    Team4 finished dblclick edit StickyNote Event with FrontEnd

--- team4/FrontEnd/src/components/Board.vue
+++ team4/FrontEnd/src/components/Board.vue
@@ -1,13 +1,6 @@
 <template>
         <!-- <button @click="drawARect">畫圖</button> -->
         <div><button id="createStickyNoteButton" @click="createStickyNote()">Add New StickyNote</button>
-
-<!--          <div>-->
-<!--            content: <input type="text" placeholder="" v-model="figurecontent"><br>-->
-
-<!--          </div>-->
-
-<!--        <div><button id="editStickyNoteButton" @click="editStickyNote('7d9813e1-6360-4b58-a333-b1935af350d2')">Edit StickyNote</button></div>-->
           <canvas id="canvas" ref='board' ></canvas></div>
 
 </template>
@@ -36,60 +29,15 @@ export default {
     // this.timer = setInterval(this.refreshCanvas, 10000)
   },
   methods: {
-    // figurecontent: undefined,
     async getBoardContent () {
       try {
-        this.boardId = '7b7d5bdb-d6d5-41f1-b32a-93f694c03811'
+        this.boardId = 'bd92c152-07a0-49bf-ab0a-42ec35314d2d'
         const res = await axios.get('http://localhost:8081/boards/' + this.boardId + '/content')
         console.log(res.data)
-        // return res.data
         this.drawStickyNote(res.data.figureDtos)
       } catch (err) {
         console.log(err)
       }
-    },
-    initCanvas () {
-      this.canvas = new fabric.Canvas('canvas', {
-        width: window.innerWidth,
-        height: window.innerHeight
-      })
-    },
-
-    drawStickyNote (figureDtos) {
-      var _this = this
-      figureDtos.forEach(figure => {
-        console.log(figure)
-        console.log(figure.style)
-        var rect = new fabric.Rect({
-          originX: 'center',
-          originY: 'center',
-          width: figure.style.width,
-          height: figure.style.height,
-          fill: figure.style.color
-        })
-        var text = new fabric.IText(figure.content, {
-          originX: 'center',
-          originY: 'center'
-        }
-        )
-
-        var group = new fabric.Group([rect, text], {
-          color: figure.style.color,
-          content: figure.content,
-          id: figure.figureId,
-          left: figure.position.x,
-          top: figure.position.y
-        })
-
-        // console.log(rect.extraData.figureId)
-        _this.canvas.add(group)
-        // _this.canvas.add(text)
-      })
-      this.canvas.renderAll()
-    },
-    refreshCanvas () {
-      this.canvas.clear()
-      this.getBoardContent()
     },
     async createStickyNote () {
       try {
@@ -103,28 +51,26 @@ export default {
             style: {
               fontSize: 12,
               shape: 2,
-              width: 100.0,
-              height: 100.0,
-              color: '#f9f900'
+              width: 150.0,
+              height: 150.0,
+              color: '#ffa150'
             }
           }
         )
         console.log(res.data.message)
-        // return res.data
       } catch (err) {
         console.log(err)
       }
-      // this.refreshCanvas()
+      this.refreshCanvas()
     },
     async editStickyNote (figure) {
       try {
-        console.log(figure)
         const res = await axios.post('http://localhost:8081/board/' + this.boardId + '/editStickyNote',
           {
             figureId: figure.get('id'),
             content: figure.get('content'),
             style: {
-              fontSize: 50,
+              fontSize: figure.item(1).get('fontSize'),
               shape: 2,
               width: parseFloat(figure.width) * parseFloat(figure.get('scaleX')),
               height: parseFloat(figure.height) * parseFloat(figure.get('scaleY')),
@@ -136,32 +82,120 @@ export default {
       } catch (err) {
         console.log(err)
       }
+      this.refreshCanvas()
+    },
+    async moveStickyNote (figure) {
+      // try {
+      //   const res = await axios.post('http://localhost:8081/board/' + this.boardId + '/editStickyNote',
+      //       {
+      //         figureId: figure.get('id'),
+      //         position: figure
+      //       }
+      //   )
+      //   console.log(res.data.message)
+      // } catch (err) {
+      //   console.log(err)
+      // }
       // this.refreshCanvas()
+    },
+    initCanvas () {
+      this.canvas = new fabric.Canvas('canvas', {
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      // this.canvas.setBackgroundColor('gray')
+    },
+    drawStickyNote (figureDtos) {
+      var _this = this
+      figureDtos.forEach(figure => {
+        var shadow = new fabric.Shadow({
+          color: 'gray',
+          blur: 8
+        })
+        var rect = new fabric.Rect({
+          id: figure.figureId,
+          originX: 'center',
+          originY: 'center',
+          width: figure.style.width,
+          height: figure.style.height,
+          fill: figure.style.color,
+          shadow: shadow
+        })
+        var text = new fabric.IText(figure.content, {
+          fontSize: figure.style.fontSize,
+          originX: 'center',
+          originY: 'center'
+        }
+        )
+        var group = new fabric.Group([rect, text], {
+          id: figure.figureId,
+          content: figure.content,
+          color: figure.style.color,
+          top: figure.position.y,
+          left: figure.position.x,
+          subTargetCheck: true
+        })
+        _this.canvas.add(group)
+      })
+      this.canvas.renderAll()
+    },
+    refreshCanvas () {
+      this.canvas.clear()
+      // this.canvas.setBackgroundColor('gray')
+      this.getBoardContent()
     },
     listenEventsOnCanvas () {
       var _this = this
       var canvas = this.canvas
       canvas.on(
         {
-          'mousedblclick': function (e) {
-            console.log('object:mousedblclick')
-            e.target.opacity = 0.5
+          'mouse:dblclick': function (e) {
+            console.log('object:dblclick')
+            var oldleft = e.target.left
+            var oldtop = e.target.top
+            var rect = e.target.item(0)
+            var dimensionText = e.target.item(1)
+            console.log('rect:', rect)
+            _this.ungroup(e.target)
+            canvas.setActiveObject(dimensionText)
+            dimensionText.enterEditing()
+            dimensionText.selectAll()
+            dimensionText.on('editing:exited', function () {
+              var group = new fabric.Group([rect, dimensionText], {
+                color: rect.fill,
+                content: dimensionText.text,
+                id: rect.id,
+                left: oldleft,
+                top: oldtop
+              })
+              canvas.remove(rect)
+              canvas.remove(dimensionText)
+              console.log('current:', canvas.getObjects())
+              canvas.add(group)
+              _this.editStickyNote(group)
+            })
           },
           'object:selected': function (e) {
-            console.log('object:selected')
-            e.target.opacity = 0.5
           },
           'object:scaled': function (e) {
             console.log('object:scaled')
             _this.editStickyNote(e.target)
           },
           'object:moved': function (e) {
-            console.log('object:moved')
             _this.moveStickyNote(e.target)
           }
-
-
         })
+    },
+    ungroup (group) {
+      console.log('ungroup flow')
+      var _this = this
+      var items = group._objects
+      group._restoreObjectsState()
+      _this.canvas.remove(group)
+      for (var i = 0; i < items.length; i++) {
+        _this.canvas.add(items[i])
+      }
+      _this.canvas.renderAll()
     }
   }
 
