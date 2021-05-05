cd0e31dc45b265d420e3a488da81052284c748d9
zxjte9411 <a5852241@gmail.com>
2021-05-05 17:07:23 +0800

    Team 3 added resize sticky note usecase

---
+++ team3/ezMiro/board/src/main/java/ntut/csie/selab/usecase/widget/resize/ResizeStickyNoteUseCaseOutput.java
@@ -0,0 +1,15 @@
+package ntut.csie.selab.usecase.widget.resize;
+
+import ntut.csie.selab.entity.model.widget.Coordinate;
+
+public class ResizeStickyNoteUseCaseOutput {
+    private Coordinate coordinate;
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
+}
