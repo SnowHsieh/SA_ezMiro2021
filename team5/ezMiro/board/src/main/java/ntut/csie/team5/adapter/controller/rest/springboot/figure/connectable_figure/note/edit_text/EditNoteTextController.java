41080d4295127376921530d85952c3960c7bc05e
109598022 <willy870501@gmail.com>
2021-05-05 19:07:36 +0800

    team5 finish frontend

--- team5/ezMiro/board/src/main/java/ntut/csie/team5/adapter/controller/rest/springboot/figure/connectable_figure/note/edit_text/EditNoteTextController.java
+++ team5/ezMiro/board/src/main/java/ntut/csie/team5/adapter/controller/rest/springboot/figure/connectable_figure/note/edit_text/EditNoteTextController.java
@@ -4,15 +4,13 @@ import ntut.csie.sslab.ddd.adapter.presenter.cqrs.CqrsCommandPresenter;
 import ntut.csie.sslab.ddd.adapter.presenter.cqrs.CqrsCommandViewModel;
 import ntut.csie.team5.usecase.figure.connectable_figure.note.edit_text.EditNoteTextInput;
 import ntut.csie.team5.usecase.figure.connectable_figure.note.edit_text.EditNoteTextUseCase;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.ws.rs.QueryParam;
 
 @RestController
 public class EditNoteTextController {
--- team5/ezMiro/board/src/main/java/ntut/csie/team5/adapter/controller/rest/springboot/figure/connectable_figure/note/post/PostNoteController.java
+++ team5/ezMiro/board/src/main/java/ntut/csie/team5/adapter/controller/rest/springboot/figure/connectable_figure/note/post/PostNoteController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.QueryParam;
 import java.awt.*;
 
 @RestController
