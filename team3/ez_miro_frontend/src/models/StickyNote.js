539bf38411deec3f6ddeec19d00d05941a1470c1
zxjte9411 <a5852241@gmail.com>
2021-05-05 20:38:59 +0800

    Team 3 added delete API adjusted the stretch point on sticky note

--- team3/ez_miro_frontend/src/models/StickyNote.js
+++ team3/ez_miro_frontend/src/models/StickyNote.js
@@ -3,9 +3,22 @@ import { fabric } from 'fabric'
 fabric.StickyNote = fabric.util.createClass(fabric.Rect, {
   type: 'stickynote',
   id: null,
+  text: 'owo',
   initialize: function (element, options) {
     options = options || {}
     this.callSuper('initialize', element, options)
     this.id = element.id
+    this.setControlsVisibility({
+      bl: true, // 左下
+      br: true, // 右下
+      mb: false, // 下中
+      ml: false, // 中左
+      mr: false, // 中右
+      mt: false, // 上中
+      tl: true, // 上左
+      tr: true, // 上右
+      mtr: true // 旋轉控制鍵
+    })
+    this.text = element.text
   }
 })
