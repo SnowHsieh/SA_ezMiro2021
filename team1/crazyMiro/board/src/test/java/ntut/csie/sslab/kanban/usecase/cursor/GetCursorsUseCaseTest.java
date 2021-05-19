b1e2c4ad464cd2570ea7c1b7709cc6296202bf34
Larry <clps20710@gmail.com>
2021-05-19 19:53:33 +0800

    Added GetCursorUseCaseTest class.

---
+++ team1/crazyMiro/board/src/test/java/ntut/csie/sslab/kanban/usecase/cursor/GetCursorsUseCaseTest.java
@@ -0,0 +1,12 @@
+package ntut.csie.sslab.kanban.usecase.cursor;
+
+import ntut.csie.sslab.kanban.usecase.AbstractSpringBootJpaTest;
+import org.junit.jupiter.api.Test;
+
+public class GetCursorsUseCaseTest extends AbstractSpringBootJpaTest {
+
+    @Test
+    public void get_cursors() {
+
+    }
+}
