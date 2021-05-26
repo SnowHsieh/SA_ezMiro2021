dcab96f1220165b222344f2ce6d576bf8dca1fb9
jing xun <t105590021@ntut.org.tw>
2021-05-26 16:22:29 +0800

    Auto stash before rebase of "team3/zxj/dev"

--- team3/ezMiro/board/src/main/java/ntut/csie/selab/entity/model/widget/event/WidgetMoved.java
+++ team3/ezMiro/board/src/main/java/ntut/csie/selab/entity/model/widget/event/WidgetMoved.java
@@ -6,13 +6,19 @@ import java.util.Date;
 
 public class WidgetMoved extends DomainEvent {
     private String widgetId;
+    private String boardId;
 
-    public WidgetMoved(Date occurredOn, String widgetId) {
+    public WidgetMoved(Date occurredOn, String boardId, String widgetId) {
         super(occurredOn);
+        this.boardId = boardId;
         this.widgetId = widgetId;
     }
 
     public String getWidgetId() {
         return widgetId;
+    }
+
+    public String getBoardId() {
+        return boardId;
     }
 }
